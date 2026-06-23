5885767cad1f456dc308017404b559056c5f3085
Ahmad Idris <aidris1@charlotte.edu>
2026-06-22 23:14:32 -0400

    Add GameManager save loading support

--- src/Services/GameManager.cs
+++ src/Services/GameManager.cs
@@ -1,13 +1,38 @@
 using Godot;
 using System;
 using Gridfall.Domain;
+using Gridfall.Services;
 
 public partial class GameManager : Node
 {
+	public CharacterBase CurrentCharacter { get; private set; }
+
+	private SaveService _saveService = new SaveService();
+
 	public override void _Ready()
 	{
-		GD.Print("Game Manager Ready");
+		GD.Print("!!!!!!!! GAME MANAGER READY !!!!!!!!");
 		CharacterBase.OnPlayerDeath += HandlePlayerDeath;
+		LoadCharacterData();
+	}
+
+	public void LoadCharacterData()
+	{
+		if (_saveService.SaveExists())
+		{
+			SaveData saveData = _saveService.Load();
+
+			CurrentCharacter = new CharacterBase();
+			CurrentCharacter.LoadFromSave(saveData);
+
+			GD.Print("GameManager loaded saved character data.");
+			GD.Print(CurrentCharacter.CharacterName);
+			GD.Print(CurrentCharacter.Level);
+		}
+		else
+		{
+			GD.Print("GameManager found no save data.");
+		}
 	}
 
 	public override void _Process(double delta)
